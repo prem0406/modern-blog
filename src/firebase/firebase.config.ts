b8df1c8192830fcfc4dd03c075aad5af63f31ea2
Prem Kumar <prem.kumar@nagarro.com>
2026-02-17 10:48:37 +0530

    update firebase config

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,24 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBGqaa_iQCcd-LE6TooRmR90eIgD91yv0g",
-  authDomain: "modern-blog-firestore.firebaseapp.com",
-  projectId: "modern-blog-firestore",
-  storageBucket: "modern-blog-firestore.firebasestorage.app",
-  messagingSenderId: "895607747581",
-  appId: "1:895607747581:web:f93371b387b29b96475f94",
-  measurementId: "G-JGDKGKNVPR",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
